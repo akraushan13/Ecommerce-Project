e514c7aa318a2c1042abda5c5e92150c9e9b8e1e
Avinash <akraushan13@gmail.com>
2023-03-11 17:21:29 +0530

    Product Databases add

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -18,6 +18,7 @@ from .private import email_id , email_password
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 STATIC_DIR = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 
 # Quick-start development settings - unsuitable for production
@@ -135,6 +136,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
